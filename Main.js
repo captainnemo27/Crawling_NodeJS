483d46e693d8014e65c034785eff6a6d2ddd5e62
VanThangDao <vanthang.dao@officience.com>
2021-08-09 00:06:39 +0700

    edit main.js Download

--- Main.js
+++ Main.js
@@ -1,47 +1,55 @@
 const Apify = require('apify');
 const fs = require('fs');
-// const remoteDataset = await Apify.openDataset('my-remote-data', { forceCloud: true });
-// Apify.main(async () => {
-//     const requestQueue = await Apify.openRequestQueue();
-//     await requestQueue.addRequest({ url: 'https://www.facebook.com/' });
+
+Apify.main(async () => {
+    const requestQueue = await Apify.openRequestQueue();
+    await requestQueue.addRequest({ url: 'https://dothi.net/nha-dat-ban/p5.htm\'' });
+
+    const crawler = new Apify.PuppeteerCrawler({
+        requestQueue,
+        handlePageFunction: async ({ request, page}) => {
+            const title = await page.title();
+            console.log(`Title of ${request.url}: ${title}`);
+
+            const {body} = await Apify.utils.requestAsBrowser(request);
+            // const dataset = await Apify.openDataset('my-results');
+            fs.writeFile("HTM/p5.htm", body, 'utf8' ,(err) => {
+                   if (err) throw err;
+            });
+            }
+
+    });
+
+    await crawler.run();
+});
+// Prepare a list of URLs to crawl
+// Prepare a list of URLs to crawl
+// let proxy = '172.16.1.11:3128';
 //
-//     const crawler = new Apify.PuppeteerCrawler({
-//         requestQueue,
-//         handlePageFunction: async ({ request, page}) => {
-//             const title = await page.title();
-//             console.log(`Title of ${request.url}: ${title}`);
-//             await Apify.utils.enqueueLinks({
-//                requestQueue,
-//                page,
-//                pseudoUrls: ['https://www.facebook.com/'],
+// Apify.main(async () => {
+//     const requestList = new Apify.RequestList({
+//         sources: [{url: 'https://dothi.net/nha-dat-ban/p*.htm'}],
+//     });
+//
+//
+//     await requestList.initialize();
+//
+// // Crawl the URLs
+//     const crawler = new Apify.BasicCrawler({
+//
+//         requestList,
+//         handleRequestFunction: async ({request }) => {
+//             // 'request' contains an instance of the Request class
+//             // Here we simply fetch the HTML of the page and store it to a dataset
+//             // const usedProxyUrl = proxyInfo.url;
+//             const {body} = await Apify.utils.requestAsBrowser(request);
+//             // const dataset = await Apify.openDataset('my-results');
+//             fs.writeFile("HTM/p3.htm", body, 'utf8' ,(err) => {
+//                 // throws an error, you could also catch it here
+//                 if (err) throw err;
 //             });
 //         },
 //     });
 //
 //     await crawler.run();
 // });
-// Prepare a list of URLs to crawl
-// Prepare a list of URLs to crawl
-Apify.main(async () => {
-    const requestList = new Apify.RequestList({
-        sources: [{url: 'https://dothi.net/nha-dat-ban/p2.htm'}],
-    });
-    await requestList.initialize();
-
-// Crawl the URLs
-    const crawler = new Apify.BasicCrawler({
-        requestList,
-        handleRequestFunction: async ({request}) => {
-            // 'request' contains an instance of the Request class
-            // Here we simply fetch the HTML of the page and store it to a dataset
-            const {body} = await Apify.utils.requestAsBrowser(request);
-            const dataset = await Apify.openDataset('my-results');
-            fs.writeFile("p2.htm", body, 'utf8' ,(err) => {
-                // throws an error, you could also catch it here
-                if (err) throw err;
-            });
-        },
-    });
-
-    await crawler.run();
-});
